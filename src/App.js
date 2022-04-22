56b20004a79b91669c8845ac7b1fe5d1896ecc02
nguyetdoan <minhnguyetdoan057@gmail.com>
2022-04-22 19:51:42 +0700

    Merge dev/nguyet to main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import React from 'react';
-import Sidebar from './components/sidebar/Sidebar';
+import "./App.css";
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
 import ProductPage from "./components/Products/ProductPage";
 
 function App() {
   return (
     <div className="App" id="outer-container">
-      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
       <div id="page-wrap">
         <h1>This is Navbar</h1>
+        <ProductPage />
       </div>
     </div>
   );
